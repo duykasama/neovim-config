9f2b623ef31da5ac0acf10cccf14609b58da808c
duykasama <duykasama@gmail.com>
2024-01-01 11:31:05 +0700

    add configurations

---
+++ lua/duy/packer.lua
@@ -0,0 +1,40 @@
+-- This file can be loaded by calling `lua require('plugins')` from your init.vim
+
+-- Only required if you have packer configured as `opt`
+vim.cmd [[packadd packer.nvim]]
+
+return require('packer').startup(function(use)
+	-- Packer can manage itself
+	use 'wbthomason/packer.nvim'
+
+	use {
+		'nvim-telescope/telescope.nvim', tag = '0.1.5',
+		-- or                            , branch = '0.1.x',
+		requires = { {'nvim-lua/plenary.nvim'} }
+	}
+
+	require('packer').startup(function(use)
+		use({ 'rose-pine/neovim', as = 'rose-pine' })
+	end)
+
+	use ('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
+	use ('ThePrimeagen/harpoon')
+	use {
+		'VonHeikemen/lsp-zero.nvim',
+		requires = {
+			{'neovim/nvim-lspconfig'},
+			{'williamboman/mason.nvim'},
+			{'williamboman/mason-lspconfig.nvim'},
+
+			{'hrsh7th/nvim-cmp'},
+			{'hrsh7th/cmp-buffer'},
+			{'hrsh7th/cmp-path'},
+			{'saadparwaiz1/cmp_luasnip'},
+			{'hrsh7th/cmp-nvim-lsp'},
+			{'hrsh7th/cmp-nvim-lua'},
+
+			{'L3MON4D3/LuaSnip'},
+			{'rafamadriz/friendly-snippets'},
+		}
+	}
+end)
